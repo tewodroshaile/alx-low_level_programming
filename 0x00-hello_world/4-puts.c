dae32cec1a7945758b49b37b57cf2b68177b5999
Your Name <you@example.com>
2023-02-17 22:06:31 -0800

    done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-the main function returns the sentence
+ *
+ * Return:0
+ */
+int main(void)
+{
+char word[50] = "\"Programming is like building a multilingual puzzle";
+puts(word);
+return (0);
+}
