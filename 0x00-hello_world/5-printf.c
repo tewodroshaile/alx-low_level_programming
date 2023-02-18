7e49a3f3f49718e7e0890bfc73f2e90fbf075cf7
Your Name <you@example.com>
2023-02-17 22:21:03 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main: write a string using printf function
+ * main - write a string using printf function
  *
  * Return:0
  */
-int main()
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n";
 	return (o);
