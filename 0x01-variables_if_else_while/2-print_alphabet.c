34cfc4006f01983e2016b247b7c3738f61ee041c
Your Name <you@example.com>
2023-02-19 05:56:45 -0800

    lowercase print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - this program functions to print an alphabet on lowercase
+ * return :0 (shows the success of the program)
+ **/
+int main(void)
+{
+char c = 't';
+
+putchar(tolower(c));
+return (0);
+}
