b7333cde44b4e993c602d67af1abe9c9fe29195f
Your Name <you@example.com>
2023-02-19 11:33:41 -0800

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - the strting point of the program tha prints the number from 0-9
+ *
+ * Return: 0 returns the output
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
