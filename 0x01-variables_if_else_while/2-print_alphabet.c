7b4cf9a2303252e1d149638c268fbcd07cfa667c
Your Name <you@example.com>
2023-02-19 06:23:05 -0800

    m

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * main -his program functions to print an alphabet on lowercase
- * return :0 (shows the success of the program)
+ * main - his program functions to print an alphabet on lowercase
+ * return:0 (shows the success of the program)
  **/
 int main(void)
 {
-char c;
+char c = ' ';
 
 for (c = 'a' ; c <= 'z' ; c++)
 {
