b99ebe4d7fdf96035ba61738a2b8202dfe3128bf
Your Name <you@example.com>
2023-02-20 06:00:06 -0800

    hexadecimal number done

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,7 +7,7 @@ int main(void)
 {
 int n;
 char c;
-for (n = 0; n < 9; n++)
+for (n = 48; n <= 57; n++)
 {
 putchar(n);
 }
@@ -15,5 +15,6 @@ for (c = 'a'; c <= 'f'; c++)
 {
 putchar(c);
 }
+putchar('\n');
 return (0);
 }
