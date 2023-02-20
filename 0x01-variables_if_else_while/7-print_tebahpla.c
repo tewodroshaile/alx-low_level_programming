b6283ea0796ee1f947e1881d97056c3aa7c0c7ec
Your Name <you@example.com>
2023-02-20 01:13:14 -0800

    z-a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - it prints the lowercase alpabets from z-a
+ * Return: 0 it shows the success of the program
+ */
+int main(void)
+{
+char c;
+
+for (c = 'z'; c <= 'a'; c--)
+{
+putchar(tolower(c));
+}
+putchar('\n');
+}
