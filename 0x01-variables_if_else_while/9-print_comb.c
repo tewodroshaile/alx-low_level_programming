5e408dfab6bee5f1f10754aa5ce48c8b6e506b3f
Your Name <you@example.com>
2023-02-20 06:25:17 -0800

    coma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - list of numbers from 0-9 with coma delimation
+ * Return: 0 the success message
+ */
+int main(void)
+{
+int n;
+for (n = 48; n < 58; n++)
+{putchar(n);
+if (n != 57)
+{
+putchar(' ');
+putchar(',');
+}
+}
+return (0);
+}
