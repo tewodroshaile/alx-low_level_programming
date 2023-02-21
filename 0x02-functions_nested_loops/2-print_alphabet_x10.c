afee3342a6717375795d7dcf2aaec4faa5b8df26
Your Name <you@example.com>
2023-02-21 13:09:46 -0800

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 * main - check the code.
-* print_alphabet_x10 -prints the alphabet a-z
+* print_alphabet_x10 - prints the alphabet a-z
 * Return: Always 0.
 */
 void print_alphabet_x10(void)
