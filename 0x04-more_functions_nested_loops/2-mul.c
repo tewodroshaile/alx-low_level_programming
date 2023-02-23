8b6fa4402bda1f79f1ce1b9f46aacc6d5bb65a4e
Your Name <you@example.com>
2023-02-23 13:29:56 -0800

    mil

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - checks for checks for a digit
+ * @x:  Variable
+ * @d:  variable
+ * Return: Always 0.
+ */
+int mul(int x, int d)
+{
+int mul;
+
+mul = x * d;
+return (mul);
+}
+
