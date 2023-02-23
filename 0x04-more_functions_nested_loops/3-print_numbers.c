769d1a68ae5dd1fa8b3c5470887418728387512b
Your Name <you@example.com>
2023-02-23 13:34:20 -0800

    done

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+ int x;
+
+for (x = 48; x < 58; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
+
