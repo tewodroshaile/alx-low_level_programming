7375f6ea0c41c8b7bf9935cb21a07fdcade2757f
Your Name <you@example.com>
2023-02-25 02:24:09 -0800

    last digit of the number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - it prints the last digit of the number
+ * @x: the requestd number
+ * Return: it returns the last digit of the number
+ */
+int print_last_digit(int x)
+{
+x = x % 10;
+return (x);
+}
