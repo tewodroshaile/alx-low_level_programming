9e3d15df36d4b998cd9be6c86e5c26b789a56285
Your Name <you@example.com>
2023-02-26 11:17:54 -0800

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - a function adding 2 numbers
+ * Description : reciving 2 numbers from main function and callculates the sum
+ * @x : the first nunber
+ * @y : the second number
+ * Return : 0 returnd the the sum of both numbers
+ */
+
+int add(int x, int y)
+{
+int sum;
+
+sum = x + y;
+return (sum);
+}
