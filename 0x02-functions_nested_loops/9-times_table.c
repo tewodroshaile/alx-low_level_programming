16a18510883f7570568c0698224cedeff036cab7
Your Name <you@example.com>
2023-02-26 01:29:58 -0800

    hour

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void times_table(void)
+{
+int i, j;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+if
+_putchar(i);
+_putchar(',');
+}
+_putchar(i);
+}
+}
