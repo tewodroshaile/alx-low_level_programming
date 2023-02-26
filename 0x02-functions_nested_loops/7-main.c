61dbdc35ccad42b2d1cecc0d15410e679174fa9f
Your Name <you@example.com>
2023-02-25 21:54:57 -0800

    last digit

--- 0x02-functions_nested_loops/7-main.c
+++ 0x02-functions_nested_loops/7-main.c
@@ -7,12 +7,9 @@
  */
 int main(void)
 {
-int r;
-
 print_last_digit(98);
 print_last_digit(0);
-r = print_last_digit(-1024);
-_putchar('0' + r);
+print_last_digit(-1024);
 _putchar('\n');
 return (0);
 }
