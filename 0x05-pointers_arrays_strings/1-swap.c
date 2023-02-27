e464f689b86414b4d090f9d9d2ba792b3926c10d
Your Name <you@example.com>
2023-02-27 11:20:31 -0800

    fir

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swap 2 numbers
+ * @a: thes first number
+ * @b: second number
+ *
+ * Discription: swap two nymbers using pointer variable
+ */
+void swap_int(int *a, int *b)
+{
+int *c;
+
+c = a;
+*a = c;
+}
