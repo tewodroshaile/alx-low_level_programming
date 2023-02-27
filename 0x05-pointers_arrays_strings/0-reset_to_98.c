49cb9be322f2190a0aa402dd135cb6a8a9f2455b
Your Name <you@example.com>
2023-02-27 10:18:22 -0800

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - change the value of a parameter using pointer variable
+ * @*n: a pointer variable
+ * Discription: the function thp change a value of a variable while called
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
