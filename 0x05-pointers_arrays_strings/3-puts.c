7eb55fbef74d4133e5bc6ee3801c8b7398587e10
Your Name <you@example.com>
2023-02-28 03:11:37 -0800

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - is a function prints each character of the a sstring
+ *@str: a parameter that holds a string
+ *
+ * Description: a function prints a contrnt of string variable using poiter
+ */
+void _puts(char *str)
+{
+int i;
+
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
