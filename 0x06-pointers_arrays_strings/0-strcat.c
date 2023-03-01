c41e0d2d22ebd775f176940b48fdc1cd2d006c32
Your Name <you@example.com>
2023-03-01 02:57:01 -0800

    concatnate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - a function which concatinates two chars
+ * @dest: the first string
+ * @src: the second character
+ *
+ * Description: acept two collection of chars and print the concatinate
+ * Return: returns the concatinated data
+ */
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+return (dest);
+}
