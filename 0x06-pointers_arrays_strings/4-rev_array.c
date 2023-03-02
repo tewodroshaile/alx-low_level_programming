8d07aa2277b3720951e5f9f11c2426d945d5a948
Your Name <you@example.com>
2023-03-02 11:45:16 -0800

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "_putchar.c"
+/**
+ * reverse_array - a funtonion that prints a value in reverse order.
+ * @a: an array of string
+ * @n: an intiger that shows the size of an array.
+ * Description: reverse_array
+ */
+void reverse_array(int *a, int n)
+{
+int i, j;
+
+for (i = 0; i < n--; i++)
+{
+j = a[i];
+a[i] = a[n];
+a[n] = j;
+}
+}
