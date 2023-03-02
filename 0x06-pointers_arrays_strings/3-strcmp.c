c8e576a2c85edc8b8f69121422f594bb70115bdf
Your Name <you@example.com>
2023-03-02 09:08:26 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,16 @@
 #include "main.h"
+#include <string.h>
 /**
- *
- *
- *
+ * _strcmp - a string comparison function
+ * @s1: the first passed string
+ * @s2: the second string
+ * Description: this function compares two strings using strcmp()
+ * Return: the comparison output
  */
 int _strcmp(char *s1, char *s2)
 {
+int i;
 
+i = strcmp(s1, s2);
+return (i);
 }
