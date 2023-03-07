6fe38e39d398072da2ebc09643089531d2e87df1
Your Name <you@example.com>
2023-03-07 01:36:02 -0800

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - a function that acepts the addres of S1 and the value of S0
+ * @s: the first passd variable
+ * @to: the second passed variable
+ *
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
