f91d6140cbe19f6ad40921569409b69add3a1f73
Your Name <you@example.com>
2023-03-08 07:06:20 -0800

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * _puts_recursion - a recursion function
+ * @s: a char parametr that will be recurssion
+ *
+ */
+void _puts_recursion(char *s)
+{
+printf("%s \n", s);
+}
