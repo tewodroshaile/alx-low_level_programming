aec00ee9f0d6813f86b4064f35e24cbd0cb85406
Your Name <you@example.com>
2023-03-13 06:45:06 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - the main function to return number of arguments
+ * @argc: the first argument
+ * @argv: the second argument
+ * Return: returns the succuss of the program
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+(void)argv;
+return (0);
+}
