030dbd9c9b5766b1aff427cd9e17118954c162a2
Your Name <you@example.com>
2023-03-13 04:27:58 -0700

    mynameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - the main function to return its name
+ * @argc : the first parameter
+ * @argv : the second paramerer
+ * Return : shows the success of the function
+ */
+int main(int argc, char *argv[])
+{
+printf("%s", argv[0]);
+return argc;
+}
