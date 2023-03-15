8893610d099f44031e6bc6d236d1426613ac1a75
Your Name <you@example.com>
2023-03-15 11:41:31 -0700

    100-changes

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * main - prints the minimum number of coins to
- * make change for an amount of money
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
- */
+* main - prints the minimum number of coins to
+* make change for an amount of money
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: 0 (Success), 1 (Error)
+*/
 int main(int argc, char *argv[])
 {
 int num, j, result;
