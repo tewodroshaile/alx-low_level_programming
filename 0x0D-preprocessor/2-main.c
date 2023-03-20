176260538b7b8abc4f0011eab39df6c7eeb2f674
Your Name <you@example.com>
2023-03-20 11:39:44 -0700

    __file__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *main - a function it prints its name
+ *Return: returns the succuss
+ */
+int main(void)
+{
+printf("%s\n", __FILE__);
+
+return (0);
+}
