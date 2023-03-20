e699406f9ced8b3b2e34e59755659b287cd27866
Your Name <you@example.com>
2023-03-20 12:43:46 -0700

    dogs struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog - dog's info
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
