8159dd81726806ab07882147dcca299aa9d90072
Your Name <you@example.com>
2023-03-21 10:52:30 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+	#include "dog.h"
+/**
+ *free_dog - a function
+ *@d: a parameter
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
+}
