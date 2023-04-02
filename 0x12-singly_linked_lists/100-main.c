a8938e53acced112821d2aae2f565132834c23d8
Your Name <you@example.com>
2023-04-02 07:52:40 -0700

    print a string

--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-_print_it();	
-printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+_print_it();
+printf("(A tortoise, having pretty good sense of a hare's nature, ");
+printf("challenges one to a race.)\n");
 return (0);
 }
