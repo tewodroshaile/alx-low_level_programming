3e4fda32bd1255d2eedccf1e04f4af9d28e4d7e6
Your Name <you@example.com>
2023-04-02 07:34:03 -0700

    print a string

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *_print_it - a function that prints a string
+ */
+void _print_it();
+void _print_it()
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+_print_it();	
+printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+return (0);
+}
