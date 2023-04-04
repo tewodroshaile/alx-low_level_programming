234fddd81b527eb6c460d5a0d15a8e1802395922
Your Name <you@example.com>
2023-04-04 12:19:10 -0700

    add _first

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,8 +12,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new = malloc(sizeof(listint_t));
 
-new = NULL;
-if (*head == NULL)
+if (new == NULL)
 	return (NULL);
 
 new->n = n;
