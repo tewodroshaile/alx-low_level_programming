035e9c98981018e0b5cbadc930faa990393fba6e
Your Name <you@example.com>
2023-04-04 13:19:50 -0700

    add_first

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * add_nodeint_end - a function to add a node
+ * add_nodeint - a function to add a node
  * @head: the first parameter
  * @n: the second parameter
  * Return: returns the out put of the function
  */
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t new = malloc(sizeof(listint_t));
+listint_t *new = malloc(sizeof(listint_t));
 
 if (new == NULL)
 	return (NULL);
