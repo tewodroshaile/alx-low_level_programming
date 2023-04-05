d80054548249b5aed6106a995592a97ce5a8cab2
Your Name <you@example.com>
2023-04-05 12:02:53 -0700

    acesses a node with index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * get_nodeint_at_index - a function to accesse with index
+ * @head: a node
+ * @index: an indix on the linked list
+ * Return: returns the indexed node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int x = 0;
+listint_t *node;
+
+node = head;
+
+while (x < index)
+{
+	node = node->next;
+	x++;
+}
+return (node);
+}
