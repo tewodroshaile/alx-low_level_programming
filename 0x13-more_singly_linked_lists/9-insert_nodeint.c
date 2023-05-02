8a8b3b6332d956b00dabcaf8fc698b608dfa4c0b
Your Name <you@example.com>
2023-05-02 14:15:12 -0700

    add index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,12 @@ new_node = malloc(sizeof(listint_t));
 new_node->n = n;
 new_node->next = NULL;
 
+if (idx == 0)
+{
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
 for (i = 0; i < idx; i++)
 {
 	if (i == idx - 1)
