29ada64711cfc45b936ef4b8ea5ac3ef4db01463
Your Name <you@example.com>
2023-05-03 13:10:23 -0700

    delet

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 listint_t *temp_node = *head;
 listint_t *pass = NULL;
-unsigned int i;
+unsigned int i = 0;
 
 if (*head == NULL)
 	return (-1);
