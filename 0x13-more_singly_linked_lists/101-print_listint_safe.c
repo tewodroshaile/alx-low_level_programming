0203a6379790dbd82843b78f828e08ccbee19b9e
Your Name <you@example.com>
2023-05-03 12:53:18 -0700

    free list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint_safe - a function that prints a list
+ * @head: a pointer to a list
+ * Return: returns the output
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+
+}
