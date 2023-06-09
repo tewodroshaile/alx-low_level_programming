027b32668f51261235f4d843b7d77043b1340ef3
tewodroshaile <teddyviva800@gmail.com>
2023-06-09 06:22:27 -0700

    name argc-argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - the main function to return its name
+ * @argc: the first parameter
+ * @argv: the second paramerer
+ * Return: shows the success of the function
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+(void)argc;
+return (0);
+}
