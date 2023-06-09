0c728f9a23d76f11a72476385605ed57107bfa89
tewodroshaile <teddyviva800@gmail.com>
2023-06-09 06:27:39 -0700

    1-args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the main function to return number of arguments
+ * @argc: the first argument
+ * @argv: the second argument
+ * Return: returns the succuss of the program
+ */
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+(void)argv;
+return (0);
+}
+
